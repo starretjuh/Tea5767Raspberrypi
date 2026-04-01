d7bc20c9ad63525872dea48d2e43901f8619195b
starretjuh <122093775+starretjuh@users.noreply.github.com>
2026-04-01 12:01:46 +0200

    Update setup.py with new content

--- setup.py
+++ setup.py
@@ -4,8 +4,8 @@ setup(
     name='Tea5767Raspberrypi',
     version='0.1.0',
     author='Juh Starret',
-    author_email='youremail@example.com',
-    description='A Raspberry Pi driver for the TEA5767 radio chip.',
+    author_email='starretjuh@example.com',
+    description='A Raspberry Pi driver for the TEA5767 FM radio chip with Morse code transmitter.',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/starretjuh/Tea5767Raspberrypi',
@@ -13,16 +13,13 @@ setup(
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
-        'Operating System :: POSIX',
+        'Operating System :: POSIX :: Linux',
     ],
     install_requires=[
         'RPi.GPIO',
-        'spidev',
+        'smbus-cffi',
+        'quick2wire-api',
+        'websocket-client',
     ],
-    entry_points={
-        'console_scripts': [
-            'tea5767=tea5767:main',
-        ],
-    },
     python_requires='>=3.6',
 )
